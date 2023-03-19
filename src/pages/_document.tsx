0a4b9d942cb6db653d4eb67333e0efcb4c4e1496
Duck1s <joeri.breedveld123@gmail.com>
2023-03-19 21:34:43 +0100

    fix(build): fix title location and ts type import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, {
   Head,
   Main,
   NextScript,
-  DocumentContext,
+  type DocumentContext,
 } from "next/document";
 
 // Functions
@@ -25,7 +25,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
-          <title>Joeri Breedveld</title>
           <meta
             name="description"
             content="Joeri Breedveld is a passionate frontend developer driven by learning and challenges"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 // Imports
 import { type NextPage } from "next";
+import Head from "next/head";
 import Navbar from "~/components/common/Navbar/Navbar";
 import Hero from "~/components/sections/Hero/Hero";
 
 // Functions
 const Home: NextPage = () => (
   <>
+    <Head>
+      <title>Joeri Breedveld</title>
+    </Head>
     <header className="absolute z-20 w-full">
       <Navbar />
     </header>
