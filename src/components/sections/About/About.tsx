cb89fa86d7321c5295c07abb21afdb5b0ebdeff9
Duck1s <32653@ma-web.nl>
2023-03-27 21:32:44 +0200

    feat(about): fix about layout wtf

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 // Functions
 const About = () => (
-  <section className="relative flex bg-[#212529]">
-    <section className="section-padding-y page-width flex flex-col gap-12">
+  <section className="relative flex flex-col bg-[#212529] md:flex-row">
+    <section className="section-padding-y page-width min-w-screen flex flex-col gap-12">
       <h2 className="font-bold text-[#E5E5E5]">Get to know me</h2>
-      <p className="text-xs font-light text-[#E5E5E5] lg:max-w-[50%] lg:pr-16 xl:pr-32">
+      <p className="text-xs font-light text-[#E5E5E5] md:max-w-[50%] md:pr-16 xl:pr-32">
         Hello, my name is Joeri Breedveld. I am 18 years old and live in
         Amsterdam. I have been programming since 2021 and am studying Software
         Development at Mediacollege Amsterdam. In my free time, I work on small
@@ -16,12 +16,14 @@ const About = () => (
         also work very structured.
       </p>
     </section>
-    <Image
-      src="/img/about.jpg"
-      alt="Contact background image programming"
-      className="absolute top-0 left-0 hidden max-w-[50vw] translate-x-full transform object-cover opacity-50 lg:block"
-      fill
-    />
+    <div className="relative h-[20rem] md:absolute md:min-h-full md:w-full md:max-w-[50vw] md:translate-x-full">
+      <Image
+        src="/img/about.jpg"
+        alt="Contact background image programming"
+        className="top-0 left-0 h-[20rem] w-full transform object-cover opacity-50"
+        fill
+      />
+    </div>
   </section>
 );
 
