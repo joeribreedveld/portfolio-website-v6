dd60d655be378b1713e370d8b341b9ad132027d8
Duck1s <joeri.breedveld123@gmail.com>
2023-05-07 23:26:39 +0200

    feat(contact): change image dim to real image file dim

--- src/components/sections/Contact/Contact.tsx
+++ src/components/sections/Contact/Contact.tsx
@@ -4,16 +4,8 @@ import Image from "next/image";
 
 // Functions
 const Contact = () => (
-  <section className="section-padding-y relative mb-24 bg-[#343A40]">
-    <div className="opacity-50">
-      <Image
-        src="/img/contact-bg.jpg"
-        alt="Contact background image programming"
-        className="object-cover"
-        fill
-      />
-    </div>
-    <div className="page-width relative z-10 flex flex-col gap-12">
+  <section className="section-padding-y relative mb-24 bg-[url(/img/contact-bg.jpg)] bg-cover bg-center">
+    <div className="page-width flex flex-col gap-12">
       <section className="flex flex-col items-center gap-4">
         <h2 className="font-bold text-[#FEFEFE]">Reach out to me</h2>
         <p className="text-xs font-light text-[#E5E5E5]">
